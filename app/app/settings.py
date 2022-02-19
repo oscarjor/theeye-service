f5d24959896bbf4579c795fa0d63dadee5c1f2b6
Oscar Ortiz <oskr.jor@gmail.com>
2022-02-18 23:05:10 -0500

    Add validations example.

--- app/app/settings.py
+++ app/app/settings.py
@@ -131,7 +131,7 @@ SWAGGER_SETTINGS = {
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Los_Angeles'
 
 USE_I18N = True
 
--- app/events/serializers.py
+++ app/events/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 from events.models import Session, Event, Application, EventCategory
+from events.utils import VALIDATOR_MAP
+from django.utils import timezone
 
 
 class SessionSerializer(serializers.ModelSerializer):
@@ -26,3 +28,19 @@ class EventSerializer(serializers.ModelSerializer):
     class Meta:
         model = Event
         fields = ['session', 'category', 'name', 'data', 'timestamp']
+
+    def validate(self, data):
+        """
+        Check specific validation based on category and name and validate no future dates.
+        """
+        if data['timestamp'] > timezone.now():
+            raise serializers.ValidationError("timestamp should not be in the future")
+
+        validation_key = f"{data['category'].slug} : {data['name']}"
+        print(">>>>>", validation_key)
+        if validation_key in VALIDATOR_MAP:
+            keys = VALIDATOR_MAP.get(validation_key)
+            for key in keys:
+                if key not in data['data'].keys():
+                    raise serializers.ValidationError("the data payload is not valid for the category and name")
+        return data
